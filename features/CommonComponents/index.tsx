432a7ec497baceac9ee1d7ace8f8759523af8a66
Christopher Nguyen <chris@ampleinsight.com>
2022-07-06 19:29:30 -0400

    Initial commit

---
+++ features/CommonComponents/index.tsx
@@ -0,0 +1,3 @@
+export const CommonComponents = () => {
+	return <div>temp</div>;
+};
---
+++ pages/details/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Details: React.FC = () => {
+	return <div> details page!!!</div>;
+};
+
+export default Details;
